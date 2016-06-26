66fd93f8e05700debe28fd8cfe5068d1baed2def
Motoharu <Motoharu@MARETOL-HOME2>
2016-06-27 01:08:47 +0900

    rewrite

--- MainData.cpp
+++ MainData.cpp
@@ -1,17 +1,21 @@
+//---------------------------------------------------------------------------------
+// Written by Motoharu Someya (@maretol)
+// 2016-06-26
+//---------------------------------------------------------------------------------
 
-#include <stdio.h>
 #include <DxLib.h>
+#include <iostream>
+#include "DxData.h"
+#include "VrData.h"
+
+//---------------------------------------------------------------------------------
+// 大雑把なフローチャート
+// | start | -> | OpenVR init | -> | DxLib init | -> | DxLibによるメインループ |
+//		-> | DxLib shutdown | -> | OpenVR shutdown | -> | End |
+//---------------------------------------------------------------------------------
 
 int WINAPI WinMain(HINSTANCE hins, HINSTANCE hpreins, LPSTR lpcmdline, int nCmdShow) {
-	if (DxLib_Init() == -1) {
-		return -1;
-	}
-
-	DrawPixel(320, 240, GetColor(255, 255, 255));
-
-	WaitKey();
-
-	DxLib_End();
-
+	VrData *vd = new VrData();
+	vd->Init();
 	return 0;
 }
