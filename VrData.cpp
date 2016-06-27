d5c0268f945b9106d2272fe31ea3e378d1c23ade
Motoharu <Motoharu@MARETOL-HOME2>
2016-06-28 01:46:49 +0900

    add dx_init call

--- VrData.cpp
+++ VrData.cpp
@@ -1,15 +1,31 @@
 #include "VrData.h"
 
 VrData::VrData() {
-
+	dd = new DxData();
 }
 
 VrData::~VrData() {
-
+	delete dd;
 }
 
 void VrData::Init() {
 	// VR初期化
+	eError = vr::VRInitError_None;
+	m_HMD = vr::VR_Init(&eError, vr::VRApplication_Scene); // ここにビルドエラー LNK2019 発生
+
+	if (eError = vr::VRInitError_Init_HmdNotFound) {
+		dd->setOutputDevice(DxData::MONITOR);
+		dd->Init();
+	} else if(eError = vr::VRInitError_None) {
+		dd->setOutputDevice(DxData::HMD);
+		dd->Init();
+	} else {
+		MessageBox(NULL, "ERROR ON INIT", "ERROR : " + eError, MB_OK);
+		m_HMD = NULL;
+		vr::VR_Shutdown();
+		return;
+	}
+
 }
 
 void VrData::getHeadTracking() {
