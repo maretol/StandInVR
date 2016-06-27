f7ea17af054dc325a20c32ba22857e6fb7689832
Motoharu <Motoharu@MARETOL-HOME2>
2016-06-28 01:46:53 +0900

    add dx_init call

--- VrData.h
+++ VrData.h
@@ -1,14 +1,22 @@
 #pragma once
 #include <openvr.h>
+#include "DxData.h"
+#include <Windows.h>
 
 class VrData {
 public:
+	vr::EVRInitError eError;
+	DxData *dd;
+
 	VrData();
 	~VrData();
 	void Init();
 protected:
+	bool DebugNoHMD = true;
 
 private:
+	vr::IVRSystem *m_HMD;
+
 	void getHeadTracking();
 	void getPositionTracking();
 };
